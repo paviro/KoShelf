9a94cffc0402167ad7ec8e0f66430ea434f125d5
Paul-Vincent Roll <paviro@me.com>
2025-12-13 04:02:29 +0100

    Replaced comic icon with speech bubble

--- src/site_generator/utils.rs
+++ src/site_generator/utils.rs
@@ -111,8 +111,8 @@ impl SiteGenerator {
             items.push(NavItem {
                 label: self.translations.get("comics"),
                 href: href.to_string(),
-                // Comic book icon (layered rectangles with binding)
-                icon_svg: "M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10".to_string(),
+                // Comic book icon (speech bubble)
+                icon_svg: "M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z".to_string(),
                 is_active: current_page == "comics",
             });
         }
